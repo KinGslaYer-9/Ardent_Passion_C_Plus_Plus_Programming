2b6e32177566d83b949a2394a5cedc52fab59fa8
Gi Yun, Kim <flgofk78@gmail.com>
2020-05-24 13:09:27 +0900

    Chapter10 까지 완료(강의 수강, 예제 작성 및 문제 풀이)

---
+++ Chapter10/Question10_2_1/Question10_2_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+	int xpos, ypos;
+public:
+	Point(int x = 0, int y = 0) : xpos(x), ypos(y)
+	{ }
+	void ShowPosition() const
+	{
+		cout << '[' << xpos << ", " << ypos << ']' << endl;
+	}
+	Point operator-()
+	{
+		Point ret(xpos * -1, ypos * -1);
+		return ret;
+	}
+};
+
+int main(void)
+{
+	Point pos1(1, 2);
+	Point pos2 = -pos1;
+
+	pos1.ShowPosition();
+	pos2.ShowPosition();
+	return 0;
+}
---
+++ Chapter10/Question10_2_2/Question10_2_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+	int xpos, ypos;
+public:
+	Point(int x = 0, int y = 0) : xpos(x), ypos(y)
+	{ }
+	void ShowPosition() const
+	{
+		cout << '[' << xpos << ", " << ypos << ']' << endl;
+	}
+	Point operator~()
+	{
+		Point ret(~xpos, ~ypos);
+		return ret;
+	}
+};
+
+int main(void)
+{
+	Point pos1(1, 2);
+	Point pos2 = ~pos1;
+	pos2.ShowPosition();
+	return 0;
+}
